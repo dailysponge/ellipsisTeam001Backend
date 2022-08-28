f9ef72b8348616e750ff4fa053bfd13a614cb7d3
Justin Wang <ajustinwang@gmail.com>
2022-08-28 18:17:44 +0800

    Create deposit and withdrawal route

--- src/routes/transaction/index.js
+++ src/routes/transaction/index.js
@@ -1,14 +1,53 @@
 const express = require('express');
 const moment = require('moment');
-
+const { transaction }= require("../../utils")
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     try {
         if (!req.body.userId || !req.body.deposit) {
             throw new Error('Missing parameters');
         }
-    } catch (error) {}
+        const userId = req.body.userId;
+        const deposit = req.body.deposit;
+        const [createTransactionError, createTransaction] = await transaction.createTransaction(userId, deposit);
+        if (createTransactionError) throw new Error(createTransactionError);
+
+        const response = {
+            status: 200,
+            timestamp: moment().format(),
+            data: {
+                createTransaction
+            }
+        };
+        res.json(response);
+    } catch (error) {
+        console.error('Error creating transaction', error);
+        res.status(500).json('Error creating transaction');
+    }
 });
 
+router.patch('/', async (req, res) => {
+    try {
+        if (!req.body.userId || !req.body.deposit) {
+            throw new Error('Missing parameters');
+        }
+        const userId = req.body.userId;
+        const deposit = req.body.deposit;
+        const [updateTransactionError, updateTransaction] = await transaction.updateTransaction(userId, deposit);
+        if (updateTransactionError) throw new Error(updateTransactionError);
+
+        const response = {
+            status: 200,
+            timestamp: moment().format(),
+            data: {
+                updateTransaction
+            }
+        };
+        res.json(response);
+    } catch (error) {
+        console.error('Error updating transaction', error);
+        res.status(500).json('Error updating transaction');
+    }
+})
 module.exports = router;
--- src/utils/transaction.js
+++ src/utils/transaction.js
@@ -9,6 +9,16 @@ module.exports = {
             };
             let transaction = new transactionModel(doc);
             transaction = await transaction.save();
+            return [null, transaction];
+        } catch (error) {
+            console.error("Error getting user's asset report", error);
+            return [error, null];
+        }
+    },
+    updateTransaction: async (userId,amount) => {
+        try{
+            const transaction = await transactionModel.findOneAndUpdate({userId}, {$inc: {deposit: amount} }, {new:true}).exec();
+            return [undefined, transaction];
         } catch (error) {
             console.error("Error getting user's asset report", error);
             return [error, null];
